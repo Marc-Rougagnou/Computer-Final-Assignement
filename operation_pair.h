623d97078e9ceae02c58ba5d17b7d408091f0f56
MarcRG <marc.rougagnou@efrei.net>
2023-12-03 20:38:14 -0500

    first try but don't test

--- operation_pair.h
+++ operation_pair.h
@@ -6,7 +6,12 @@
 #define COMPUTER_FINAL_ASSIGNEMENT_OPERATION_PAIR_H
 
 #include "function_main.h"
-void STR(operationInfo lineInfo, char stack[],int sp, char registers[], char variables[], int pc);
-
+void STR(operationInfo lineInfo, char registers[], char variables[]);
+void POP(operationInfo lineInfo, char stack[],int sp, char registers[]);
+void OR(operationInfo lineInfo, char registers[], char variables[]);
+void ADD(operationInfo lineInfo, char registers[],char variables[]);
+void DIV(operationInfo lineInfo, char registers[], char variables[]);
+void MOD(operationInfo lineInfo, char registers[], char variables[]);
+void DEC(operationInfo lineInfo, char registers[], char variables[]);
 
 #endif //COMPUTER_FINAL_ASSIGNEMENT_OPERATION_PAIR_H
